c9b5904a103fca49d20de4ba4e059a0855f5b0e5
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2016-06-28 19:36:59 -0700

    Add middleware that adds thrift-source-paths to source-paths
    Now we don't need to add `src/thrift` to the source-paths of every service

--- lein-finagle-clojure/src/lein_finagle_clojure/plugin.clj
+++ lein-finagle-clojure/src/lein_finagle_clojure/plugin.clj
@@ -1,5 +1,11 @@
 (ns lein-finagle-clojure.plugin
-  (:require [leiningen.finagle-clojure]))
+  (:require [leiningen.finagle-clojure]
+            [leiningen.core.project :as p]))
 
 (defn hooks []
   (leiningen.finagle-clojure/activate))
+
+(defn middleware [project]
+  (if-let [thrift-source-path (get-in project [:finagle-clojure :thrift-source-path])]
+    (update-in project [:source-paths] conj thrift-source-path)
+    project))
