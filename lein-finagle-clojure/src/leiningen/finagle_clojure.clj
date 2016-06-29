c70c008ecef4b1ad269041638c76f210b1ad5d41
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2016-06-28 19:13:36 -0700

    Add friendly error when source thrift file is not resolved

--- lein-finagle-clojure/src/leiningen/finagle_clojure.clj
+++ lein-finagle-clojure/src/leiningen/finagle_clojure.clj
@@ -3,6 +3,7 @@
             [robert.hooke]
             [clojure.java.io :as io]
             [leiningen.core.classpath :as classpath]
+            [leiningen.core.main :as lein]
             [cemerick.pomegranate :as pom])
   (:import (java.io File)
            (java.net URL)))
@@ -29,7 +30,7 @@
   (let [target-dir (File. project-root target-temp-dir)
         target-file (File. target-dir filename)]
     (.mkdirs target-dir)
-    (leiningen.core.main/info
+    (lein/info
      "Copying" (.getPath thrift-url) "to" (.getPath target-file))
     (spit target-file (slurp thrift-url))))
 
@@ -62,7 +63,7 @@
         source-path (get-in project [:finagle-clojure :thrift-source-path])
         raw-dest-path (get-in project [:finagle-clojure :thrift-output-path])]
     (if-not (and source-path raw-dest-path)
-      (leiningen.core.main/info "No config found for lein-finagle-clojure, not compiling Thrift for" (:name project))
+      (lein/info "No config found for lein-finagle-clojure, not compiling Thrift for" (:name project))
       (let [absolute-dest-path (->> raw-dest-path (io/file project-root) (.getAbsolutePath))
             thrift-files (find-thrift-files project-root source-path)
             include-filenames (->> thrift-files
@@ -73,7 +74,9 @@
                                                     thrift-files)))
             _ (doseq [filename include-filenames
                       :let [resource (io/resource filename)]]
-                (copy-file-to-target-temp-dir project-root resource filename))
+                (if-not resource
+                  (lein/abort (str "Aborting, could not find thrift source file: " filename))
+                  (copy-file-to-target-temp-dir project-root resource filename)))
             include-dir (.getPath (File. project-root target-temp-dir))
             scrooge-args (concat ["--finagle" "--skip-unchanged" "--language" "java" "--dest" absolute-dest-path]
                                  ["-i" include-dir]
@@ -82,10 +85,10 @@
           (let [default-args ["--disable-rule" "Namespaces"]
                 additional-args (rest options)
                 linter-args (concat default-args additional-args thrift-files)]
-            (leiningen.core.main/info "Linting Thrift files:" thrift-files)
+            (lein/info "Linting Thrift files:" thrift-files)
             (com.twitter.scrooge.linter.Main/main (into-array String linter-args))))
-        (leiningen.core.main/info "Compiling Thrift files:" thrift-files)
-        (leiningen.core.main/debug "Calling scrooge with parameters:" scrooge-args)
+        (lein/info "Compiling Thrift files:" thrift-files)
+        (lein/debug "Calling scrooge with parameters:" scrooge-args)
         (com.twitter.scrooge.Main/main (into-array String scrooge-args))))))
 
 (defn javac-hook
